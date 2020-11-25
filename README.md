6ef45b542c241e0ce37bfb76d49a9f45fe4880a0
Ryan Freese <ryan.p.freese@gamil.com>
2020-11-24 18:52:54 -0600

    adding files

--- README.md
+++ README.md
@@ -68,3 +68,30 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+------------------------------
+
+# Responsive Layout - Now With React:
+This project contains a responsive layout of a portfolio page. Bootstrap frameworking is used.
+
+See here:
+
+[Responsive Layout](https://keepthelidontight.github.io/Responsive_Portfolio/index.html)
+<br>
+[GitHub Repository](https://github.com/KeepTheLidOnTight/Responsive_Portfolio)
+
+# Usage Instructions
+Click on the links in the navbar to view different pages of the site. Resize the browser window to see the responsive layout.
+
+# Installation:
+N/A
+
+# Credits/Thanks:
+Bootstrap was used to design this responsive layout. Google fonts also referenced.
+
+# License Information: 
+N/A
+
+# Furthur Developments/Edits
+-Continue to customize elements of the portfolio
+
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -19,41 +19,28 @@ function Projects() {
                 </div>
                 <div className="col mb-4">
                 <div className="card">
-                    <img src="https://github.com/samergain/work-day-scheduler/raw/master/workday.png" className="card-img-top" alt="screenshot of work-day-scheduler project" />
+                    <img src="workdayscreenshot.png" className="card-img-top" alt="screenshot of work-day-scheduler project" />
                     <div className="card-body">
-                    <h5 className="card-title">Word Day Scheduler</h5>
-                    <a href="https://github.com/samergain/work-day-scheduler" target="_blank" rel="noreferrer">9-5 work-day tasks</a>
-                    <p className="card-text">Simple interface for scheduling your workday. Enter your task in a time-slot and click save button to have it saved in your browser.</p>
+                    <h5 className="card-title">Day Planner</h5>
+                    <a href="https://keepthelidontight.github.io/dailyPlanner/" target="_blank" rel="noreferrer">Workday Planner</a>
                     </div>
                 </div>
                 </div>
                 <div className="col mb-4">
                 <div className="card">
-                    <img src="https://github.com/samergain/weather-app/raw/main/largeLayout.png" className="card-img-top" alt="screenshot of a weather app" />
+                    <img src="weatherboard.png" className="card-img-top" alt="screenshot of a weather app" />
                     <div className="card-body">
-                    <h5 className="card-title">Weather Application</h5>
-                    <a href="https://github.com/samergain/weather-app" target="_blank" rel="noreferrer">Search for a city to check the weather</a>
-                    <p className="card-text">Search for a city in the U.S. get current weather and 5 days forecast for that city. All searched cities are stored in localStorage and the last one entered witll be on screen.</p>
+                    <h5 className="card-title">Weather Dashboard</h5>
+                    <a href="https://keepthelidontight.github.io/weatherDashboard/" target="_blank" rel="noreferrer">Weather Dashboard</a>
                     </div>
                 </div>
                 </div>
                 <div className="col mb-4">
                 <div className="card">
-                    <img src="https://samergain.github.io/samers-portfolio/images/whales.png" className="card-img-top" alt="Whales Tracker project screenshot" />
+                    <img src="artists.png" className="card-img-top" alt="Whales Tracker project screenshot" />
                     <div className="card-body">
-                    <h5 className="card-title">Whales Tracker</h5>
-                    <a href="https://github.com/samergain/Whale_Tracker" target="_blank" rel="noreferrer">Whales Tracker</a>
-                    <p className="card-text">Intergrating two APIs to locate whales sightings around the world. Geo search and species search available.</p>
-                    </div>
-                </div>
-                </div>
-                <div className="col mb-4">
-                <div className="card">
-                    <img src="https://samergain.github.io/samers-portfolio/images/robofriends.png" className="card-img-top" alt="screenshot of RoboFriends project" />
-                    <div className="card-body">
-                    <h5 className="card-title">Welcome React!</h5>
-                    <a href="https://github.com/samergain/robofriends" target="_blank" rel="noreferrer">RoboFriends</a>
-                    <p className="card-text">Find your robo-friend with my first project using React.</p>
+                    <h5 className="card-title">Artist's In The News</h5>
+                    <a href="https://tsinmoua.github.io/Artists-in-the-News/" target="_blank" rel="noreferrer">Artist's In the News</a>
                     </div>
                 </div>
                 </div>
